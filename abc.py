746c0bb6507e8b53c33fcaa790483ad447f66549
amit-arabhavi <74947757+amit-arabhavi@users.noreply.github.com>
2021-10-19 20:33:22 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,11 @@
+a=float(input("my 1st no :"))
+b=int(input("my 2nd no :"))
+#c=3.14*a*a
+#print("total is =",c) #print calcualate of area.
+
+x = lambda a, b: a * b
+
+y= lambda c, d: c/d
+
+print(x(5,2))
+print(y(10,2))
